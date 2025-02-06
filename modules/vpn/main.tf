fbbe33386bd641f589ab247e62446f77a6bb6789
RichieTheDev <111089062+RichieTheDev@users.noreply.github.com>
2025-02-06 20:31:26 +0100

    Update main.tf

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -1,5 +1,3 @@
-Certainly! Here’s your Terraform configuration with detailed comments explaining each resource:
-
 # Create an AWS Direct Connect Gateway
 # This enables a dedicated network connection between on-premises infrastructure and AWS.
 resource "aws_dx_gateway" "dx_gateway" {
@@ -13,39 +11,3 @@ resource "aws_vpn_connection" "vpn_connection" {
   customer_gateway_id = aws_customer_gateway.customer_gateway.id  # Reference to the on-premises VPN gateway
   type                = "ipsec.1"  # Specifies that this is an IPSec VPN connection
 }
-
-# Create an S3 Bucket for Storing Transferred Files
-# This bucket will store files that are uploaded from the on-premises system.
-resource "aws_s3_bucket" "file_storage" {
-  bucket = "onprem-file-storage-bucket"  # Unique name for the S3 bucket
-}
-
-Explanation of Key Components
-
-1. Direct Connect Gateway (aws_dx_gateway)
-
-Acts as the connection point between on-prem and AWS.
-
-Uses BGP ASN (Autonomous System Number) for routing traffic.
-
-
-
-2. VPN Connection (aws_vpn_connection)
-
-Establishes a secure IPSec tunnel between on-premises and AWS.
-
-The customer_gateway_id refers to an on-prem VPN device (defined separately).
-
-
-
-3. S3 Bucket (aws_s3_bucket)
-
-Used for storing files transferred from on-premises to AWS.
-
-Can be secured with encryption and access policies if needed.
-
-
-
-
-This setup ensures secure and reliable file transfer from on-prem infrastructure to AWS S3. Let me know if you need any modifications or enhancements!
-
