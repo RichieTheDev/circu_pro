599e65342fda30e0f6deeaac5afc12d82db37152
RichieTheDev <111089062+RichieTheDev@users.noreply.github.com>
2025-02-06 20:30:11 +0100

    Update main.tf

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -1,34 +1,51 @@
-# Create a Direct Connect Gateway
-resource "aws_dx_gateway" "vpn_gateway" {
-  name            = "terraform_ipsec_vpn_gateway"
-  amazon_side_asn = "64512"
+Certainly! Here’s your Terraform configuration with detailed comments explaining each resource:
+
+# Create an AWS Direct Connect Gateway
+# This enables a dedicated network connection between on-premises infrastructure and AWS.
+resource "aws_dx_gateway" "dx_gateway" {
+  name            = "onprem_to_aws_dx_gateway"  # Name of the Direct Connect Gateway
+  amazon_side_asn = "64512"  # ASN (Autonomous System Number) assigned by AWS for BGP routing
 }
 
-# Create a Transit Gateway for network routing
-resource "aws_ec2_transit_gateway" "vpn_transit_gateway" {
-  amazon_side_asn             = "64513"
-  description                 = "VPN and Direct Connect Transit Gateway"
-  transit_gateway_cidr_blocks = ["10.0.0.0/24"]
-}
-
-# Create a Customer Gateway (on-premises VPN endpoint)
-resource "aws_customer_gateway" "customer_gateway" {
-  bgp_asn    = 64514
-  ip_address = "10.0.0.1"
-  type       = "ipsec.1"
-}
-
-# Associate Direct Connect Gateway with Transit Gateway
-resource "aws_dx_gateway_association" "vpn_gateway_association" {
-  dx_gateway_id         = aws_dx_gateway.vpn_gateway.id
-  associated_gateway_id = aws_ec2_transit_gateway.vpn_transit_gateway.id
-
-  allowed_prefixes = ["10.0.0.0/8"]
-}
-
-# Establish VPN connection
+# Establish a VPN Connection to AWS
+# This VPN connection allows secure communication between on-premises infrastructure and AWS.
 resource "aws_vpn_connection" "vpn_connection" {
-  customer_gateway_id = aws_customer_gateway.customer_gateway.id
-  transit_gateway_id  = aws_ec2_transit_gateway.vpn_transit_gateway.id
-  type                = "ipsec.1"
+  customer_gateway_id = aws_customer_gateway.customer_gateway.id  # Reference to the on-premises VPN gateway
+  type                = "ipsec.1"  # Specifies that this is an IPSec VPN connection
 }
+
+# Create an S3 Bucket for Storing Transferred Files
+# This bucket will store files that are uploaded from the on-premises system.
+resource "aws_s3_bucket" "file_storage" {
+  bucket = "onprem-file-storage-bucket"  # Unique name for the S3 bucket
+}
+
+Explanation of Key Components
+
+1. Direct Connect Gateway (aws_dx_gateway)
+
+Acts as the connection point between on-prem and AWS.
+
+Uses BGP ASN (Autonomous System Number) for routing traffic.
+
+
+
+2. VPN Connection (aws_vpn_connection)
+
+Establishes a secure IPSec tunnel between on-premises and AWS.
+
+The customer_gateway_id refers to an on-prem VPN device (defined separately).
+
+
+
+3. S3 Bucket (aws_s3_bucket)
+
+Used for storing files transferred from on-premises to AWS.
+
+Can be secured with encryption and access policies if needed.
+
+
+
+
+This setup ensures secure and reliable file transfer from on-prem infrastructure to AWS S3. Let me know if you need any modifications or enhancements!
+
